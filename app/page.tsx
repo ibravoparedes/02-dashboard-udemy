733a5fe63cac8caf81384889b6fee26f21cd17a8
ibravoparedes <01.bravoparedes@gmail.com>
2026-03-22 23:33:31 -0300

    primer commit subidad proyecto

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  
+  redirect('dashboard/counter');
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        <h1 className="text-4xl font-bold text-gray-900 dark:text-white"> Hola Mundo </h1>
+      </main>
+    </div>
+  );
+}
